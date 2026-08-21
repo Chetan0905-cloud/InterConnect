af739d0b7cb6d4f1dd018208af87db8d0af82c46
Chetan Debrani <chetandebrani09@gmail.com>
2026-08-21 21:12:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from .models import create_user
+from  models import create_user
 from .database import get_db_connection
 
 app = Flask(__name__)
